747ba8e2f1bb573e64dfb2708fe9ed8cfcaf980e
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-10-18 23:08:01 +0000

    [Add]2-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  resources :post_images, only: [:new, :index, :show]
   get 'about' => "homes#about", as: 'about'
 end
